3a0c227c37d05952ac7f326e1b6ed328f1b8d44b
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-11-09 12:28:52 -0500

    Update DL_setACLpermissions.ps1

--- scripts/DL_setACLpermissions.ps1
+++ scripts/DL_setACLpermissions.ps1
@@ -1,7 +1,7 @@
 $ctx = New-AzStorageContext -StorageAccountName '809rgdl' -UseConnectedAccount
 $filesystemName = "809rgdlfs"
 $dirname = "blubb/"
-$userID = "82f15ac5-8c5f-4e45-987f-fa1686da3a36";
+$entityID = "82f15ac5-8c5f-4e45-987f-fa1686da3a36";
 $permission = "r-x";
-$acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $userID -Permission $permission
+$acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $entityID -Permission $permission
 Update-AzDataLakeGen2AclRecursive -Context $ctx -FileSystem $filesystemName -Path $dirname -Acl $acl
