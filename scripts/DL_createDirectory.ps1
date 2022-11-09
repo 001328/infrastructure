017f3be03f40f545b5e1c4d2d7b9664e18fe5924
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-11-09 12:50:23 -0500

    Update DL_createDirectory.ps1

--- scripts/DL_createDirectory.ps1
+++ scripts/DL_createDirectory.ps1
@@ -1,1 +1,1 @@
-az storage fs directory create -n blubb -f 809rgdlfs --account-name 809rgdl --auth-mode login
+az storage fs directory create -n test1328 -f 809rgdlfs --account-name 809rgdl --auth-mode login
