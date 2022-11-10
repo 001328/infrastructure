f31d5053fe2a7ad360113fdb278c4c7c8dba5f11
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-11-10 18:36:01 -0500

    ggg

--- scripts/Synapse_Permissions.ps1
+++ scripts/Synapse_Permissions.ps1
@@ -1,7 +1,1 @@
-$ctx = New-AzStorageContext -StorageAccountName '809rgdl' -UseConnectedAccount
-$filesystemName = "809rgdlfs"
-$dirname = "test1328/"
-$entityID = "82f15ac5-8c5f-4e45-987f-fa1686da3a36";
-$permission = "rw-";
-$acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $entityID -Permission $permission
-Update-AzDataLakeGen2AclRecursive -Context $ctx -FileSystem $filesystemName -Path $dirname -Acl $acl
+Get-AzSynapseRoleAssignment -WorkspaceName 809rgsyn
