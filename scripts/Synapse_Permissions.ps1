15a191b77beca3b81512c685cc7029b4bc4aaec7
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-11-11 16:01:49 -0500

    Update Synapse_Permissions.ps1

--- scripts/Synapse_Permissions.ps1
+++ scripts/Synapse_Permissions.ps1
@@ -1,4 +1,4 @@
 $entityID = "63542052-ac02-4705-903a-1250a1c35f89";
 $roleID = "7572bffe-f453-4b66-912a-46cc5ef38fda";
-New-AzSynapseRoleAssignment -WorkspaceName 809rgsyn -RoleDefinitionId $roleID -ObjectId $entityID principalType User
+New-AzSynapseRoleAssignment -WorkspaceName 1111rgsyn -RoleDefinitionId $roleID -ObjectId $entityID principalType User
   
