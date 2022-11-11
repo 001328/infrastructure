aac8db1837203f9c356f72996c40e68b7ea9db97
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-11-10 19:13:45 -0500

    Update Synapse_Permissions.ps1

--- scripts/Synapse_Permissions.ps1
+++ scripts/Synapse_Permissions.ps1
@@ -1,1 +1,4 @@
-Get-AzSynapseRoleAssignment -WorkspaceName 809rgsyn
+New-AzSynapseRoleAssignment
+  -WorkspaceName azuredataplatformsyn 
+  -RoleDefinitionId 6e4bf58a-b8e1-4cc3-bbf9-d73143322b78 
+  -ObjectId 63542052-ac02-4705-903a-1250a1c35f89
