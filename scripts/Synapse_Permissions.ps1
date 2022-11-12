dfabbfbe61da44e8d296dcdfcb7cb7cd366f7003
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-11-11 22:33:28 -0500

    Update Synapse_Permissions.ps1

--- scripts/Synapse_Permissions.ps1
+++ scripts/Synapse_Permissions.ps1
@@ -1,4 +1,4 @@
 $workspace = "1113rgsyn";
-$role = "pache Spark Administrator";
+$role = "Apache Spark Administrator";
 $entityId = "63542052-ac02-4705-903a-1250a1c35f89";
 az synapse role assignment create --workspace-name $workspace --role $role --assignee-object-id $entityId --assignee-principal-type User
