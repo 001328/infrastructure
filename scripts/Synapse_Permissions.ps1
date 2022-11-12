672fbbd3e8fbf6d28f27008b33df2295c1435776
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-11-11 22:04:34 -0500

    Update Synapse_Permissions.ps1

--- scripts/Synapse_Permissions.ps1
+++ scripts/Synapse_Permissions.ps1
@@ -1,4 +1,4 @@
 $rolename = "Synapse User";
 $signInName = "hendrik.kampert@diginata.com";
-New-AzSynapseRoleAssignment -WorkspaceName 1113rgsyn -SignInName $signInName -RoleDefinitionName $rolename
+New-AzSynapseRoleAssignment -WorkspaceName 1113rgsyn -RoleDefinitionName $rolename -SignInName $signInName
  
