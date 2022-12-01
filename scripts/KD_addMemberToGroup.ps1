d698580332759c85b2b9035decdeeed2e5f5967c
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-12-01 11:46:50 -0500

    1146

---
+++ scripts/KD_addMemberToGroup.ps1
@@ -0,0 +1,8 @@
+param(
+    [Parameter()]
+    [string]$user
+)
+$group = 'Test2';
+$user = 'batman@diginata.com';
+$userId = az ad user show --id $user --query 'id';
+az ad group member add --group $group --member-id $userId
