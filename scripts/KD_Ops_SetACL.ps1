265c602154b4301da4f1a5763da3132456c47fa1
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-12-02 15:23:22 -0500

    1523

--- scripts/KD_Ops_SetACL.ps1
+++ scripts/KD_Ops_SetACL.ps1
@@ -5,7 +5,7 @@ param(
     [string]$group,
     [string]$permission
 )
-$ctx = New-AzStorageContext -StorageAccountName $resource -UseConnectedAccount
+$ctx = New-AzStorageContext -StorageAccountName $resource -UseConnectedAccount;
 $groupId = az ad group show --group $group --query 'id';
-$acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $groupId -Permission $permission
+$acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $groupId -Permission $permission;
 Update-AzDataLakeGen2AclRecursive -Context $ctx -FileSystem $container -Path $folder -Acl $acl
