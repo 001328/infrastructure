64a2480c08195157a18e59477cb5691a57803905
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-12-02 15:52:33 -0500

    Update test_DL_setACLpermissions.ps1

--- scripts/test_DL_setACLpermissions.ps1
+++ scripts/test_DL_setACLpermissions.ps1
@@ -1,6 +1,6 @@
 $ctx = New-AzStorageContext -StorageAccountName '1117rgdl' -UseConnectedAccount
 $filesystemName = "1117rgdlfs"
-$dirname = "test1328/"
+$dirname = "test001/"
 $entityID = "82f15ac5-8c5f-4e45-987f-fa1686da3a36";
 $permission = "rw-";
 $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $entityID -Permission $permission
