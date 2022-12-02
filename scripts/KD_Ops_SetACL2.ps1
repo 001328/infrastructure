2eb45dc2d8b528a4279ef774a59578108dc74cb5
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-12-02 17:07:03 -0500

    Update KD_Ops_SetACL2.ps1

--- scripts/KD_Ops_SetACL2.ps1
+++ scripts/KD_Ops_SetACL2.ps1
@@ -1,4 +1,4 @@
-$group = 'Producer001';
+$group = 'Producer001'
 $groupId = az ad group show --group $group --query 'id'
 $ctx = New-AzStorageContext -StorageAccountName '1117rgdl' -UseConnectedAccount
 $filesystemName = "1117rgdlfs"
