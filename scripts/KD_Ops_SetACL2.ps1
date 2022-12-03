be5d9cdf103bcea6c8d2b7add41b8e91eeabfc53
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-12-02 20:07:15 -0500

    Update KD_Ops_SetACL2.ps1

--- scripts/KD_Ops_SetACL2.ps1
+++ scripts/KD_Ops_SetACL2.ps1
@@ -4,8 +4,8 @@ $groupId
 $ctx = New-AzStorageContext -StorageAccountName '1117rgdl' -UseConnectedAccount
 $ctx
 $filesystemName = "1117rgdlfs"
-$dirname = "test001/"
-$entityID = "d6cc2eb7-50d8-4a3d-bb6b-77054f7a6096"
+$dirname = "test1328/"
+$entityID = $groupId
 $permission = "rw-"
 $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType user -EntityId $entityID -Permission $permission
 Update-AzDataLakeGen2AclRecursive -Context $ctx -FileSystem $filesystemName -Path $dirname -Acl $acl
