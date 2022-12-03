0bfdfbe37af0ab87220ff9a36e436f67a3e340e6
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-12-02 22:34:50 -0500

    2234

--- scripts/KD_Ops_SetACL2.ps1
+++ scripts/KD_Ops_SetACL2.ps1
@@ -8,7 +8,9 @@ $filesystemName = "1117rgdlfs"
 $dirname = "test1328/"
 $entityID = $groupId
 $permission = "rw-"
-$acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType group -EntityId $entityID -Permission $permission
-'acl'
-$acl
-Update-AzDataLakeGen2AclRecursive -Context $ctx -FileSystem $filesystemName -Path $dirname -Acl $acl
+#$acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType group -EntityId $entityID -Permission $permission
+#'acl'
+#$acl
+#Update-AzDataLakeGen2AclRecursive -Context $ctx -FileSystem $filesystemName -Path $dirname -Acl $acl
+$acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType group -Permission $permission -EntityID = $groupId
+Update-AzDataLakeGen2Item -ACL $acl -Context $ctx -FileSystem $filesystemName -Path $dirname
