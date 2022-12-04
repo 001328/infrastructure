3bcea5a8a6e371885029bd9a727634bda34f08c3
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-12-03 22:36:54 -0500

    2236

--- scripts/KD_Ops_SetACL2.ps1
+++ scripts/KD_Ops_SetACL2.ps1
@@ -6,4 +6,4 @@ $dirname = "test001/"
 $permission = "rw-"
 $groupId
 $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType group -EntityId $groupId.Replace("`"","") -Permission $permission
-Update-AzDataLakeGen2Item -FileSystem $filesystemName -Path $dirname -Acl $acl -Context $ctx
+Update-AzDataLakeGen2Item -FileSystem $container -Path $folder -Acl $acl -Context $ctx
