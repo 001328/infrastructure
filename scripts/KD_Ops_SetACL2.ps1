eb05c2bc12733dc05e198f543bc137c2e1420bd7
Hendrik Kampert <hendrikk0311@hotmail.de>
2022-12-05 14:51:57 -0500

    1451

--- scripts/KD_Ops_SetACL2.ps1
+++ scripts/KD_Ops_SetACL2.ps1
@@ -1,8 +1,8 @@
 $group = "Consumer001"
 $groupId = az ad group show --group $group --query "id"
 $ctx = New-AzStorageContext -StorageAccountName "1117rgdl" -UseConnectedAccount
-$filesystemName = "1117rgdlfs"
-$dirname = "test001/"
+$container = "1117rgdlfs"
+$folder = "test001/"
 $permission = "rw-"
 $groupId
 $acl = Set-AzDataLakeGen2ItemAclObject -AccessControlType group -EntityId $groupId.Replace("`"","") -Permission $permission
